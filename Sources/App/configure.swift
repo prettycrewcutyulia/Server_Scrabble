547bb2b6a187f97ce52853424913bc18d05137b1
irina <irinapechik@gmail.com>
2024-04-21 14:47:30 +0300

    added project

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,6 +18,7 @@ public func configure(_ app: Application) async throws {
     ), as: .psql)
 
     app.migrations.add(CreateTodo())
+    try await app.autoMigrate().get()
     // register routes
     try routes(app)
 }
