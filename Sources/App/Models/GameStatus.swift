4265fcf7eca52426763452505c7d6ec7aa396e07
irina <irinapechik@gmail.com>
2024-04-27 20:02:54 +0300

    added gameStatus enum

---
+++ Sources/App/Models/GameStatus.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Irina Pechik on 27.04.2024.
+//
+
+import Foundation
+
+enum GameStatus: String {
+    case NotStarted = "Not Started"
+    case Running
+    case Pause
+    case Ended
+}
