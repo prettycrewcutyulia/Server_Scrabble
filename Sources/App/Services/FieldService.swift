3345fdb02466bbc556fc046951b025bfa7cc0a28
Гудошникова Юлия Владимировна <gudoshnikovajulia@yandex.ru>
2024-04-30 00:09:46 +0300

    Схема сервиса доски

---
+++ Sources/App/Services/FieldService.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Юлия Гудошникова on 30.04.2024.
+//
+
+import Foundation
+
+enum FieldService {
+    static func wordScoring(word: [ChipsOnField]) -> Int {
+        return 0
+    }
+}
