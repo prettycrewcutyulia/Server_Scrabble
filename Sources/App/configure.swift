8656d3d5e23c796fb04f6bc31f07d711f167642d
YaroslavGamayunov <yaroslav.gamayunov@gmail.com>
2024-06-16 01:56:45 +0300

    No certificate verifiation of database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,15 +8,18 @@ import JWT
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    var clientConfig = TLSConfiguration.makeClientConfiguration()
+    
+    clientConfig.certificateVerification = .none
+    
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+            tls: .prefer(try .init(configuration: clientConfig)))
+        ), as: .psql)
     
     app.migrations.add(CreateGameRoom())
     app.migrations.add(CreateGamerIntoRoom())
